0d013347e6bfc5efbf7c3d1a486c3e79d4bf2d2f
green_man <green90man@gmail.com>
2018-09-19 00:38:08 +0300

    feat(app): add carousel.

--- public/javascript/custom.js
+++ public/javascript/custom.js
@@ -1,4 +1,11 @@
-  $(".nav a").on("click", function(){
-     $(".nav-item a").find(".active").removeClass("active");
-     $(this).addClass("active");
-  });
+$( document ).ready(function() {
+    console.log( "ready!" );
+
+
+	$(".nav a").on("click", function(){
+		$(".nav-item a").find(".active").removeClass("active");
+		$(this).addClass("active");
+	});
+
+	$('.carousel').carousel()
+});
