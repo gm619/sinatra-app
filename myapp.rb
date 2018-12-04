d511c713e1ee4f161db94054f4bf003ee471dc32
green_man <green90man@gmail.com>
2018-12-04 16:51:25 +0300

    feat(app): add nanobar as progress bar.

--- myapp.rb
+++ myapp.rb
@@ -28,3 +28,17 @@ end
 #     register Sinatra::Partial
 #   end
 # end
+#
+
+
+# What is this? I do not know.
+# def import_legacy_purchase_data(data)
+#   purchase_list = legacy_data_parser.parse_purchase_records(data)
+#   purchase_list.each do |purchase_record|
+#     customer = customer_list.get_customer(purchase_record.email_address)
+#     product  = product_inventory.get_product(purchase_record.product_id)
+#     customer.add_purchased_product(product)
+#     customer.notify_of_files_available(product)
+#     log_successful_import(purchase_record)
+#   end
+# end
--- public/javascript/custom.js
+++ public/javascript/custom.js
@@ -7,5 +7,17 @@ $( document ).ready(function() {
 		$(this).addClass("active");
 	});
 
-	$('.carousel').carousel()
+	$('.carousel').carousel();
+
+	var element = document.documentElement,
+		body = document.body,
+		scrollTop = 'scrollTop',
+		scrollHeight = 'scrollHeight',
+		progress = document.querySelector('.progress-bar'),
+		scroll;
+
+	document.addEventListener('scroll', function() {
+	  scroll = (element[scrollTop]||body[scrollTop]) / ((element[scrollHeight]||body[scrollHeight]) - element.clientHeight) * 100;
+	  progress.style.setProperty('--scroll', scroll + '%');
+	}); 
 });
